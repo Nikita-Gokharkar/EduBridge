fb9ab762c3b9ccff54f04064486eedd28214c526
Nikita Gokharkar <86715719+Nikita-Gokharkar@users.noreply.github.com>
2021-10-27 12:57:25 +0530

    Delete java-full-stack/Assignment/java/Basic programs directory

--- java-full-stack/Assignment/java/Basic programs/Enhancedforloop.java
+++
@@ -1,9 +0,0 @@
-class Enhancedforloop
-{
-public static void main(String args[])
-{
-int num[]={34,56,66,77,23,44};
-for(int i:num)
-{
-System.out.println(i);
-}}}
--- java-full-stack/Assignment/java/Basic programs/Tcs5.java
+++
@@ -1,9 +0,0 @@
-class Tcs5
-{
-int a=1;
-int b=2;
-int c=3;
-public static void main(String args[])
-{
-System.out.println(a+b+c);
-}}
